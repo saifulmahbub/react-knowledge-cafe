ac7a57ef68fef7053388b9db31845a737353adcf
saifulmahbub <saifulislammahbub83@gmail.com>
2024-09-15 15:30:22 +0200

    first commit

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,9 @@
+const Bookmarks = () => {
+  return (
+    <div className="md:w-1/3">
+      <h2>kaifal hal ya akhi??</h2>
+    </div>
+  );
+};
+
+export default Bookmarks;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import profile from "../../assets/images/profile.png";
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center p-4 m-4 border-b-2">
+      <h1 className="text-4xl font-bold">Knowledge-Cafe</h1>
+      <img src={profile}></img>
+    </div>
+  );
+};
+export default Header;
